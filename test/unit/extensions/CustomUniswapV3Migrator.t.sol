f2936085650ab8a3531e2dba40b41dfda69d929a
Franco Barpp Gomes <franco@nethermind.io>
2025-06-09 17:25:52 -0300

    test: Add more tests to CustomUniswapV3Migrator tests

--- test/unit/extensions/CustomUniswapV3Migrator.t.sol
+++ test/unit/extensions/CustomUniswapV3Migrator.t.sol
@@ -5,9 +5,12 @@ import { Test } from "forge-std/Test.sol";
 import { TestERC20 } from "@v4-core/test/TestERC20.sol";
 import { TickMath } from "@v4-core/libraries/TickMath.sol";
 import { ERC20 } from "@openzeppelin/token/ERC20/ERC20.sol";
+import { IERC721 } from "@openzeppelin/token/ERC721/IERC721.sol";
 import { CustomUniswapV3Migrator } from "src/extensions/CustomUniswapV3Migrator.sol";
+import { ICustomUniswapV3Migrator } from "src/extensions/interfaces/ICustomUniswapV3Migrator.sol";
 import { INonfungiblePositionManager } from "src/extensions/interfaces/INonfungiblePositionManager.sol";
 import { IUniswapV3Factory, IBaseSwapRouter02 } from "src/extensions/CustomUniswapV3Migrator.sol";
+import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import { SenderNotAirlock } from "src/base/ImmutableAirlock.sol";
 import {
     UNISWAP_V3_NONFUNGIBLE_POSITION_MANAGER_BASE,
@@ -15,8 +18,21 @@ import {
     WETH_BASE,
     UNISWAP_V3_ROUTER_02_BASE
 } from "test/shared/Addresses.sol";
-import { console } from "forge-std/console.sol";
-import { Constants } from "@v4-core-test/utils/Constants.sol";
+
+contract CustomUniswapV3MigratorHarness is CustomUniswapV3Migrator {
+    constructor(
+        address airlock_,
+        INonfungiblePositionManager positionManager_,
+        IBaseSwapRouter02 router,
+        address owner,
+        address dopplerFeeReceiver_,
+        uint24 feeTier_
+    ) CustomUniswapV3Migrator(airlock_, positionManager_, router, owner, dopplerFeeReceiver_, feeTier_) {}
+    
+    function getDivisibleTick(int24 tick, int24 tickSpacing, bool isUpper) external pure returns (int24) {
+        return _getDivisibleTick(tick, tickSpacing, isUpper);
+    }
+}
 
 contract CustomUniswapV3MigratorTest is Test {
     CustomUniswapV3Migrator public migrator;
@@ -25,10 +41,8 @@ contract CustomUniswapV3MigratorTest is Test {
     address constant LOCKER_OWNER = address(0xb055);
     address constant DOPPLER_FEE_RECEIVER = address(0x2222);
     address constant INTEGRATOR_FEE_RECEIVER = address(0x1111);
-    int24 constant DEFAULT_LOWER_TICK = 0;
-    int24 constant DEFAULT_UPPER_TICK = 200;
 
-    bytes public liquidityMigratorData = abi.encode(DEFAULT_LOWER_TICK, DEFAULT_UPPER_TICK, INTEGRATOR_FEE_RECEIVER);
+    bytes public liquidityMigratorData = abi.encode(INTEGRATOR_FEE_RECEIVER);
 
     function setUp() public {
         vm.createSelectFork(vm.envString("BASE_MAINNET_RPC_URL"), 31_118_046);
@@ -52,7 +66,7 @@ contract CustomUniswapV3MigratorTest is Test {
     function test_receive_RevertsWhenETHSenderNotAirlock() public {
         deal(address(0xbeef), 1 ether);
         vm.startPrank(address(0xbeef));
-        vm.expectRevert(SenderNotAirlock.selector);
+        vm.expectRevert("Only Airlock or Router");
         payable(address(migrator)).transfer(1 ether);
     }
 
@@ -77,71 +91,77 @@ contract CustomUniswapV3MigratorTest is Test {
         migrator.migrate(uint160(0), address(0x1111), address(0x2222), address(0));
     }
 
-    function test_migrate() public {
-        TestERC20 token0 = new TestERC20(1000 ether);
-        TestERC20 token1 = new TestERC20(1000 ether);
+    function test_migrate_RevertsWhenPoolDoesNotExist() public {
+        TestERC20 token0 = new TestERC20(1e30);
+        TestERC20 token1 = new TestERC20(1e30);
 
-        address pool = migrator.initialize(address(token0), address(token1), liquidityMigratorData);
-
-        // token0.transfer(address(migrator), 102_020_031_989);
-        // token1.transfer(address(migrator), 100_000_000_000);
-        token0.transfer(address(migrator), 1000 ether);
-        token1.transfer(address(migrator), 1000 ether);
-
-        // uint160 sqrtPriceX96 = 80828596741349383900952490931;
-        uint256 liquidity =
-            migrator.migrate(Constants.SQRT_PRICE_1_1, address(token0), address(token1), address(0xbeef));
-
-        assertEq(token0.balanceOf(address(migrator)), 0, "Wrong migrator token0 balance");
-        assertEq(token1.balanceOf(address(migrator)), 0, "Wrong migrator token1 balance");
-
-        assertEq(token0.balanceOf(pool), 1000 ether, "Wrong pool token0 balance");
-        assertEq(token1.balanceOf(pool), 1000 ether, "Wrong pool token1 balance");
+        token0.transfer(address(migrator), 1e28);
+        token1.transfer(address(migrator), 1e28);
+        
+        uint160 sqrtPriceX96_1_1 = 79228162514264337593543950336; // sqrt(1) * 2^96
+        vm.expectRevert(abi.encodeWithSelector(ICustomUniswapV3Migrator.PoolDoesNotExist.selector));
+        migrator.migrate(sqrtPriceX96_1_1, address(token0), address(token1), address(0xbeef));
     }
 
-    // function test_migrate_KeepsCorrectPrice() public {
-    //     TestERC20 token0 = new TestERC20(131_261_409_265_385_327_997_940);
-    //     TestERC20 token1 = new TestERC20(16_622_742_685_037);
+    function test_onERC721Received() public view {
+        bytes4 result = migrator.onERC721Received(address(0), address(0), 0, "");
+        assertEq(result, migrator.onERC721Received.selector, "Wrong selector returned");
+    }
 
-    //     uint160 sqrtPriceX96 = 3_893_493_510_706_508_098_175_185;
+    function test_getDivisibleTick() public {
+        CustomUniswapV3MigratorHarness harness = new CustomUniswapV3MigratorHarness(
+            address(this),
+            INonfungiblePositionManager(UNISWAP_V3_NONFUNGIBLE_POSITION_MANAGER_BASE),
+            IBaseSwapRouter02(UNISWAP_V3_ROUTER_02_BASE),
+            LOCKER_OWNER,
+            DOPPLER_FEE_RECEIVER,
+            FEE_TIER
+        );
+        
+        int24 tickSpacing = 200;
+        
+        // Test tick already divisible
+        assertEq(harness.getDivisibleTick(400, tickSpacing, false), 400);
+        assertEq(harness.getDivisibleTick(400, tickSpacing, true), 400);
+        
+        // Test rounding down
+        assertEq(harness.getDivisibleTick(450, tickSpacing, false), 400);
+        
+        // Test rounding up
+        assertEq(harness.getDivisibleTick(450, tickSpacing, true), 600);
+        
+        // Test zero tick
+        assertEq(harness.getDivisibleTick(0, tickSpacing, false), 0);
+        assertEq(harness.getDivisibleTick(0, tickSpacing, true), tickSpacing);
+    }
 
-    //     address pool = migrator.initialize(address(token0), address(token1), liquidityMigratorData);
+    function test_initialize_RevertsWithEmptyData() public {
+        vm.expectRevert(abi.encodeWithSelector(ICustomUniswapV3Migrator.EmptyLiquidityMigratorData.selector));
+        migrator.initialize(address(0x1111), address(0x2222), "");
+    }
 
-    //     token0.transfer(address(migrator), 13_126_140_926_538_532_799_794);
-    //     token1.transfer(address(migrator), 16_622_742_685_037);
-    //     migrator.migrate(sqrtPriceX96, address(token0), address(token1), address(0xbeef));
-    //     assertEq(token0.balanceOf(address(migrator)), 0, "Wrong migrator token0 balance");
-    //     assertEq(token1.balanceOf(address(migrator)), 0, "Wrong migrator token1 balance");
-    //     (uint112 reserve0, uint112 reserve1,) = IUniswapV2Pair(pool).getReserves();
-    //     uint256 price = uint256(reserve1) * 2 ** 192 / uint256(reserve0);
-    //     assertApproxEqRel(price, uint256(sqrtPriceX96) * uint256(sqrtPriceX96), 0.00000001e18);
-    // }
+    function test_initialize_RevertsWithZeroFeeReceiver() public {
+        bytes memory invalidData = abi.encode(address(0));
+        vm.expectRevert(abi.encodeWithSelector(ICustomUniswapV3Migrator.ZeroFeeReceiverAddress.selector));
+        migrator.initialize(address(0x1111), address(0x2222), invalidData);
+    }
 
-    // function test_migrate(uint256 balance0, uint256 balance1, uint160 sqrtPriceX96) public {
-    //     vm.skip(true);
-    //     uint256 max = uint256(int256(type(int128).max));
+    function test_poolFeeReceivers() public {
+        address token0 = address(0x3333);
+        address token1 = address(0x4444);
 
-    //     vm.assume(balance0 > 0 && balance0 <= max);
-    //     vm.assume(balance1 > 0 && balance1 <= max);
-    //     vm.assume(sqrtPriceX96 > TickMath.MIN_SQRT_PRICE && sqrtPriceX96 <= TickMath.MAX_SQRT_PRICE);
+        address pool = migrator.initialize(token0, token1, liquidityMigratorData);
+        assertEq(migrator.poolFeeReceivers(pool), INTEGRATOR_FEE_RECEIVER, "Wrong fee receiver");
+    }
 
-    //     TestERC20 token0 = new TestERC20(balance0);
-    //     TestERC20 token1 = new TestERC20(balance1);
+    function test_initialize_UsesWETHForNumeraireZero() public {
+        TestERC20 token = new TestERC20(1e30);
 
-    //     address pool = migrator.initialize(address(token0), address(token1), liquidityMigratorData);
-
-    //     token0.transfer(address(migrator), balance0);
-    //     token1.transfer(address(migrator), balance1);
-    //     uint256 liquidity = migrator.migrate(sqrtPriceX96, address(token0), address(token1), address(0xbeef));
-
-    //     assertEq(token0.balanceOf(address(migrator)), 0, "Wrong migrator token0 balance");
-    //     assertEq(token1.balanceOf(address(migrator)), 0, "Wrong migrator token1 balance");
-
-    //     assertEq(token0.balanceOf(pool), balance0, "Wrong pool token0 balance");
-    //     assertEq(token1.balanceOf(pool), balance1, "Wrong pool token1 balance");
-
-    //     uint256 lockedLiquidity = liquidity / 20;
-    //     assertEq(liquidity - lockedLiquidity, IUniswapV2Pair(pool).balanceOf(address(0xbeef)), "Wrong liquidity");
-    //     assertEq(lockedLiquidity, IUniswapV2Pair(pool).balanceOf(address(migrator.locker())), "Wrong locked liquidity");
-    // }
+        address pool = migrator.initialize(address(token), address(0), liquidityMigratorData);
+        
+        address weth = address(migrator.WETH());
+        (address token0, address token1) = address(token) < weth ? (address(token), weth) : (weth, address(token));
+        
+        assertEq(pool, IUniswapV3Factory(UNISWAP_V3_FACTORY_BASE).getPool(token0, token1, FEE_TIER), "Wrong pool");
+    }
 }
